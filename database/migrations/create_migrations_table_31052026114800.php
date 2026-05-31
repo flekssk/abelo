ca883c899a70dfa4e7ea7d016bcd577ab938e639
Aleksei Osipov <fks-sk@yandex.ru>
2026-05-31 14:21:34 +0400

    add: migrations table, total time ~ 12h

---
+++ database/migrations/create_migrations_table_31052026114800.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Application\Database\Facades\Db;
+use App\Application\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Db::getConnection()
+            ->exec(
+                $sql = "
+                    CREATE TABLE IF NOT EXISTS `migrations` (
+                        `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        `migration` VARCHAR(255) NOT NULL,
+                        `batch` INT NOT NULL
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;"
+            );
+    }
+
+    public function down(): void
+    {
+        Db::getConnection()->exec("DROP TABLE IF EXISTS `migrations`;");
+    }
+};
